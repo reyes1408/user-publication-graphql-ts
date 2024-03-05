6f8eaed7567836d8062541d38bdb9de76d5714bb
Daniel Reyes <DanielReHer@outlook.com>
2024-03-05 04:41:09 -0600

    se creó el servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hola mundo 2");
+// index.ts
+import "reflect-metadata";
+import { startServer } from './app';
+
+async function main() {
+    const app = await startServer();
+    app.listen(3000, () => {
+        console.log('Server running at http://localhost:3000/graphql');
+    });
+}
+
+main();
